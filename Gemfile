ec019940c0d94260e3ca2070b36e029cc446e685
aaronjohnsonco <aaron@aaronjohnson.co>
2015-10-13 13:36:03 -0500

    initial site build

--- Gemfile
+++ Gemfile
@@ -26,6 +26,8 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'bootstrap-sass', '~> 3.2.0'
 gem 'autoprefixer-rails'
 gem 'lato_rails'
+gem "rack-livereload", :group => :development
+gem 'guard-livereload', require: false
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
@@ -35,6 +37,7 @@ gem 'lato_rails'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
+
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
@@ -48,4 +51,3 @@ group :development do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
 end
-
---
+++ app/assets/javascripts/pages.js
@@ -0,0 +1,25 @@
+// Place all the behaviors and hooks related to the matching controller here.
+// All this logic will automatically be available in application.js.
+// You can use CoffeeScript in this file: http://coffeescript.org/\
+jQuery(function() {
+  $('#navContact').click(function() {
+    $('body').animate({scrollTop: $('#contact').offset().top}, 1500);
+    return false;
+  });
+  $('#navClients').click(function() {
+    $('body').animate({scrollTop: $('#clients').offset().top}, 1500);
+    return false;
+  });
+  $('#navAbout').click(function() {
+    $('body').animate({scrollTop: $('#about').offset().top}, 1500);
+    return false;
+  });
+  $('#navHow').click(function() {
+    $('body').animate({scrollTop: $('#how').offset().top}, 1500);
+    return false;
+  });
+  $('#navBrand').click(function() {
+    $('body').animate({scrollTop: $('#top').offset().top}, 1500);
+    return false;
+  });
+});
